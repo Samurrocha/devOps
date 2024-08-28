f2c9e6dc81fbc6b4aeb7771db9c43e2cb5dc63ea
Samurrocha <samuel.rrocha12@gmail.com>
2024-08-28 17:58:32 -0300

    consumo da API finalizado

---
+++ index.js
@@ -0,0 +1,34 @@
+
+// Função para buscar dados da API
+async function fetchUser() {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users', {
+            method: 'GET'
+        })
+
+        const users = await response.json();
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+        const tbody = document.querySelector('tbody');
+
+        // Preenchendo a tabela com dados
+        users.forEach(user => {
+            const tr = document.createElement('tr');
+            tr.innerHTML = `
+                <td>${user.id}</td>
+                <td>${user.name}</td>
+                <td>${user.email}</td>
+                <td>${user.address.zipcode}</td>
+            `;
+            tbody.appendChild(tr);
+        });
+    } catch (error) {
+        console.error('Error fetching user:', error);
+    }
+
+}
+
+fetchUser()
